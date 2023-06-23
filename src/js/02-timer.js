a8b7e4b0d604307e10b091b658b5c54c8a84cb5b
KostiZin <pancostian@gmail.com>
2023-06-23 19:58:32 -0400

    task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,12 +3,17 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const btn = document.querySelector('button');
 const timePicker = document.querySelector('#datetime-picker');
-// flatpickr(timePicker, {});
-console.log(btn);
-btn.disabled = true;
+const daysLeft = document.querySelector('[data-days]');
+const hoursLeft = document.querySelector('[data-hours]');
+const minutesLeft = document.querySelector('[data-minutes]');
+const secondsLeft = document.querySelector('[data-seconds]');
 
-// let currentDate = new Date();
-// console.log(currentDate);
+// console.log(btn);
+btn.disabled = true;
+let KEY = 'chosen-time';
+let currentTime = new Date().getTime();
+// let timeEvtValue;
+// console.log(timeEvtValue);
 
 const options = {
   position: 'auto',
@@ -28,15 +33,91 @@ flatpickr(timePicker, options);
 timePicker.addEventListener('input', handlePickTime);
 
 function handlePickTime(evt) {
-  let timeNew = new Date(evt.target.value);
-  console.log(timeNew.getTime());
-  let timeNew2 = new Date();
-  console.log(timeNew2.getTime());
+  let timeEvtValue = new Date(evt.target.value).getTime();
 
-  if (!timeNew.getTime() < timeNew2.getTime()) {
+  if (timeEvtValue < currentTime) {
+    btn.disabled = true;
+    return alert('Please choose a date in the future');
+  } else {
+    btn.disabled = false;
+    localStorage.setItem(KEY, JSON.stringify(timeEvtValue));
   }
-  return alert('Please choose a date in the future');
-  //   } else {
-  //     btn.disabled = true;
-  //   }
+
+  // console.log(convertMs(currentTime));
+
+  // if (days: 0 || ) {
+  //   clearInterval(x);
+  // document.getElementById("demo").innerHTML = "EXPIRED";
+}
+
+// console.log(localStorage);
+
+// let zeroTime = convertMs(chosenTime);
+
+// let now = setInterval(repeat, 1000);
+
+// btn.addEventListener('click', handleCountdown);
+btn.addEventListener('click', timeInterval);
+
+// function handleCountdown() {
+//   let chosenTime = JSON.parse(localStorage.getItem(KEY));
+
+//   const difference = chosenTime - currentTime;
+
+//   const newTime = convertMs(difference);
+//   // console.log(newTime);
+
+//   const { days, hours, minutes, seconds } = newTime;
+
+//   daysLeft.textContent = days;
+//   hoursLeft.textContent = hours;
+//   minutesLeft.textContent = minutes;
+//   secondsLeft.textContent = seconds;
+
+//   if (difference < 0) {
+//     clearInterval(now);
+//   }
+// }
+
+function timeInterval() {
+  let x = setInterval(function () {
+    let chosenTime = JSON.parse(localStorage.getItem(KEY));
+    let separatedTime = new Date().getTime();
+    const difference = chosenTime - separatedTime;
+
+    const newTime = convertMs(difference);
+    // console.log(newTime);
+
+    const { days, hours, minutes, seconds } = newTime;
+
+    daysLeft.textContent = days;
+    hoursLeft.textContent = hours;
+    minutesLeft.textContent = minutes;
+    secondsLeft.textContent = seconds;
+
+    if (difference < 0) {
+      clearInterval(x);
+    }
+  }, 1000);
+}
+
+// FUNCTION< DONT LOOK
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
